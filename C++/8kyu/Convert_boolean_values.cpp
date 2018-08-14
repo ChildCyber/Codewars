1b64b8b09cc4b44155d76bbe8453b062c09d7692
Mr.Child <lishunsheng2012@gmail.com>
2018-08-14 21:53:02 +0900

    add Convert_boolean_values.cpp

---
+++ C++/8kyu/Convert_boolean_values.cpp
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/53369039d7ab3ac506000467 */
+
+using namespace std;
+
+string bool_to_word(bool value)
+{
+  if (value)
+    return "Yes";
+  else
+    return "No";
+}
