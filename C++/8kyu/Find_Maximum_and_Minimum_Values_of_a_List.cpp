66e02fa9a2c916f8b94160335fd3387fa9380563
Mr.Child <lishunsheng2012@gmail.com>
2018-08-25 15:09:19 +0900

    add Find_Maximum_and_Minimum_Values_of_a_List.cpp

---
+++ C++/8kyu/Find_Maximum_and_Minimum_Values_of_a_List.cpp
@@ -0,0 +1,13 @@
+/* https://www.codewars.com/kata/577a98a6ae28071780000989 */
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int min(vector<int> list){
+    return *min_element(list.begin(), list.end());
+}
+
+int max(vector<int> list){
+    return *max_element(list.begin(), list.end());
+}
